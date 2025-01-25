2903ca493471a4c0b66631cc1dbdac16aef7eae7
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2025-01-25 23:30:49 +0200

    Building a Pipe That Sorts Items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
-import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { DatePipe, DecimalPipe } from '@angular/common';
 
 import { TemperaturePipe } from './temperature.pipe';
+import { SortPipe } from './sort.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [DatePipe, DecimalPipe, TemperaturePipe]
+  imports: [DatePipe, DecimalPipe, TemperaturePipe, SortPipe]
 })
 export class AppComponent {
   currentDate = new Date();
-  currentTemperaturs = {
+  currentTemperatures = {
     berlin: 4.2749812,
     newYork: 18.1214,
     paris: 72.1209001,
